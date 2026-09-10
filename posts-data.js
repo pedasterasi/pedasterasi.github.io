677aca74d85bf29af850806937943572473cd3b6
pedasterasi <pedasterasi.dev@gmail.com>
2026-09-10 22:25:42 +0700

    aaaaa

--- posts-data.js
+++ posts-data.js
@@ -1,22 +1,5 @@
 const posts = [
   {
-    "slug": "asd",
-    "date": "2026-09-10",
-    "dateLabel": "10 Sept 2026",
-    "tags": [],
-    "title": "asd",
-    "summary": "asdasd",
-    "cover": "one",
-    "content": [
-      {
-        "type": "unity",
-        "src": "../games/Pong/index.html",
-        "title": "title",
-        "caption": "optional caption"
-      }
-    ]
-  },
-  {
     "slug": "in-the-beningging-there-was-a-pong",
     "date": "2026-09-09",
     "dateLabel": "09 Sept 2026",
